1c457929eb9422e00f890a2272a4ab3d54d77304
Michal Chmielewski <m.chmielewski@tut.by>
2021-01-11 18:33:45 +0100

    Create build for github pages

---
+++ docs/precache-manifest.078bfe79d379ae6be10b2ec97f9f8e36.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1bbb7ea82205a336d9a590d4f6ed68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7206c44e98b365a0b68d",
+    "url": "/static/css/main.563db4d6.chunk.css"
+  },
+  {
+    "revision": "93c4fa0cacd710564ba4",
+    "url": "/static/js/2.1d18e8b8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1d18e8b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7206c44e98b365a0b68d",
+    "url": "/static/js/main.ccc9010d.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "094a443024b6093dcd3945e929d5df96",
+    "url": "/static/media/bestshop.094a4430.png"
+  },
+  {
+    "revision": "778c5ef3f48947e379bd0aca1a0aa963",
+    "url": "/static/media/currencycalculator2.778c5ef3.png"
+  },
+  {
+    "revision": "6d062fa4a9a96f3b127132e383fd4b66",
+    "url": "/static/media/giveaway.6d062fa4.png"
+  },
+  {
+    "revision": "78f0d1bad1448735f245c1fb4bd53d00",
+    "url": "/static/media/paulina.78f0d1ba.png"
+  },
+  {
+    "revision": "3e52e1e3214c3c1aa0793665381df2a9",
+    "url": "/static/media/snot.3e52e1e3.gif"
+  },
+  {
+    "revision": "12cf00a6f503f64514746901c9d15b19",
+    "url": "/static/media/wave.12cf00a6.gif"
+  }
+]);
